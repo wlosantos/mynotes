773aaf42cc547ff13dd00d36533f627b9ccbd555
wendellopes <wendelopes@msn.com>
2023-01-31 16:06:46 -0300

    create controller notes and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :notes
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
 end
