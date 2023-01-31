a5a201c8f6c33b059119a469fc28a537b36a8ea5
wendellopes <wendelopes@msn.com>
2023-01-31 13:29:15 -0300

    commit initial with adjust lint rubocop and rspec test

--- config/initializers/heroicon.rb
+++ config/initializers/heroicon.rb
@@ -2,7 +2,7 @@
 
 Heroicon.configure do |config|
   config.variant = :solid # Options are :solid and :outline
-  config.default_class = { solid: "h-5 w-5", outline: "h-6 w-6" }
+  config.default_class = { solid: 'h-5 w-5', outline: 'h-6 w-6' }
   ##
   # You can set a default class, which will get applied to every icon with
   # the given variant. To do so, un-comment the line below.
