c3791e50f89dc9fb320e7817f887feadfae81583
wendellopes <wendelopes@msn.com>
2023-01-31 14:39:12 -0300

    setting authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  layout :layout_authentication
+
+  protected
+
+  def layout_authentication
+    'authentication' if devise_controller?
+  end
 end
